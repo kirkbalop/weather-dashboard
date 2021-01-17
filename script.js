3dee88c24d7a53254c172de6aa3df91d721c93fd
Kirk <kirkbalop@gmail.com>
2021-01-17 14:54:00 -0800

    added html and jquery to hide 5day until search

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ const apiKey = "&appid=8163dffe92726c08e74d88ed012ec920";
 
 let date = new Date();
 
+$("#city-name").keypress(function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        $("#searchBtn").click();
+    }
+});
+
+$("#searchBtn").on("click", function() {
+    $("#5day-h4")
+        .removeClass("hide-forecast")
+        .addClass("show-forecast");
+})
+
 const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey
